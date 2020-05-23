93c784340b452026884b1e75142ceab0719c82af
Damian Maleno <franciscodamia.maleno@estudiants.urv.cat>
2020-05-23 13:48:22 +0200

    Esto suena bacano

---
+++ task2.py
@@ -0,0 +1,13 @@
+import pywren_ibm_cloud as pywren
+import numpy as np
+from io import StringIO
+import time
+
+__author__      = "Geovanny Risco y Damian Maleno"
+__credits__     = ["Geovanny Risco", "Damian Maleno"]
+__version__     = "1.0"
+__email__       = ["geovannyalexan.risco@estudiants.urv.cat", "franciscodamia.maleno@estudiants.urv.cat"]
+__status__      = "Developping"
+
+
+bucketname = 'damianmaleno' #nombre del bucket en el IBM cloud, 'geolacket'or 'damianmaleno'
