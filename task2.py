0290405dc835d09389b154913923a4f77a390bb8
Geovanny Risco <geovannyalexan.risco@estudiants.urv.cat>
2020-05-25 11:00:23 +0200

    master hasta punto 4

--- task2.py
+++ task2.py
@@ -12,43 +12,43 @@ __email__       = ["geovannyalexan.risco@estudiants.urv.cat", "franciscodamia.ma
 __status__      = "Developping"
 
 
-bucketname = 'damianmaleno' #nombre del bucket en el IBM cloud, 'geolacket'or 'damianmaleno'
+bucketname = 'geolacket' #nombre del bucket en el IBM cloud, 'geolacket'or 'damianmaleno'
 N_SLAVES = 10
 
 def master(id, x , ibm_cos):
     write_permision_list = []
-
+    requests_list=[]
     # 1. monitor COS bucket each X seconds
-    requested = False                           #he probado hacerlo con un metacaracter pero no deja
+    #requested = False                           #he probado hacerlo con un metacaracter pero no deja
     while True:
-        for i in range (1, N_SLAVES+1)
-            try:
-                if (ibm_cos.get_object(Bucket=bucketname, Key=f"p_write_{i}")):
-                    requested = True
-                    break
-            except:
-                print("Waiting for request.")
-        time.sleep(x);    
+        try:
+            contentsDict=ibm_cos.list_objects_v2(Bucket=bucketname, Prefix='p_write')['Contents'] #La funcion list_object devuelve un diccionario si encuentro el prefijo, sino salta una excepcion
+            #requested = True
+            break
+        except:
+            print("Waiting for request.")
+        time.sleep(x)   
+
     # 2. List all "p_write_{id}" files
-    if (requested)
-        j = 0
-        for i in range (1, N_SLAVES+1)
-            try:
-                if (ibm_cos.get_object(Bucket=bucketname, Key=f"p_write_{i}")):
-                    write_permision_list[j] = ibm_cos.get_object(Bucket=bucketname, Key=f"p_write_{i}")
-                    j++
-            except:
-                print("")    
+    nameFormat=re.compile("p_write_\d") #Comprobaremos que el fichero realmente tiene el formato adecuado
+    for file in contentsDict:
+        if (nameFormat.match(file['Key'])):
+            requests_list.append(dict(list(file.items())[:2])) #Solo guardamos los campos de "Key" y "LastModified"
 
     # 3. Order objects of the list "p_write"
+    sorted_list=sorted(requests_list, key=lambda item: item['LastModified']) #Lo ordena del mas antiguo al mas reciente
     # 4. Pop first object of the list "p_write_{id}"
+    next_slave=sorted_list.pop(0) #Pop el primero en la lista, es decir, el mas antiguo
     # 5. Write empty "write_{id}" object into COS
+
     # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
     # 7. Monitor "result.json" object each X seconds until it is updated
     # 8. Delete from COS “write_{id}”
     # 8. Back to step 1 until no "p_write_{id}" objects in the bucket
 
-    return write_permision_list
+    return next_slave
+    #return sorted_list
+    #return write_permision_list
 
 
 def slave(id, x, ibm_cos):
@@ -84,8 +84,16 @@ def my_function(bucketname, key, ibm_cos):
     #data= json.dumps(result)
     #ibm_cos.put_object(Bucket=bucketname, Key="obj2.json", Body=data)
     ibm_cos.put_object(Bucket=bucketname, Key=key)
-    result = ibm_cos.get_object(Bucket=bucketname, Key='p_write_{*}')
-    return result
+    #p=re.compile("p_write_\d")
+    contentsDict=ibm_cos.list_objects_v2(Bucket=bucketname, Prefix='p_write')['Contents']
+    name=re.compile("p_write_\d")
+    ok=False
+    for file in contentsDict:
+        if (name.match(file['Key'])):
+            ok=True
+            break
+    result = ibm_cos.get_object(Bucket=bucketname, Key=key)
+    return contentsDict
     #return json.loads(ibm_cos.get_object(Bucket=bucketname, Key="obj2")['Body'].read())
 
     
@@ -94,8 +102,10 @@ def my_function(bucketname, key, ibm_cos):
 if __name__ == '__main__':
 
     pw = pywren.ibm_cf_executor()
-    pw.call_async(my_function, [bucketname, 'p_write_{id}'])
+    #pw.call_async(my_function, [bucketname, 'p_write_7'])
+    pw.call_async(master, 0)
     print(pw.get_result())
+    pw.clean()
 
     # pw.call_async(master, 0)
     # pw.map(slave, range(N_SLAVES))
